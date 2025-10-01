a8a7560c1341b53738bae7ac765c4a374155c1a9
Munkh-Iveel <iveelv968@gmail.com>
2025-10-01 14:24:07 +0900

    test

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ const PORT = process.env.PORT || 4000;
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port gs ${PORT}.`);
 });
