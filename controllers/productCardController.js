9cb29af9d8b93ec897388d7063915ac2a56a1ebe
Munkh-Iveel <iveelv968@gmail.com>
2025-10-29 16:39:30 +0900

    experiment

--- controllers/productCardController.js
+++ controllers/productCardController.js
@@ -1,5 +1,6 @@
 import productCard from "../models/ProductCard.js";
 
+
 export const createProductCard = async (req, res) => {
     try {
         const { id, type, title, price, description, category, stock, discount } = req.body;
